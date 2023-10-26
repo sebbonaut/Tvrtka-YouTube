27f15a473f49a169daa0d2b1b15dfe650876ff09
sebas <sebas@DESKTOP-S92RB8H>
2023-10-26 09:15:21 +0200

    Add project files.

---
+++ Tvrtka/Osoba.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tvrtka
+{
+    public abstract class Osoba
+    {
+        public abstract string Uloga { get; } 
+        public string ime, prezime;
+        public Osoba(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+        public Osoba() : this("John","Doe") { }
+        public virtual void Info()
+            => Console.WriteLine(ime + " " + prezime);
+    }
+
+    public /* sealed */ class Zaposlenik : Osoba
+    {
+        public override string Uloga => "Zaposlenik";
+        public string titula = "prof.";
+        public /* sealed */ override void Info()
+        {
+            Console.Write(titula + " ");
+            base.Info();
+        }
+            //=> Console.WriteLine($"{titula} {ime} {prezime}");
+        public Zaposlenik(string ime, string prezime, string titula)
+            : base(ime, prezime) 
+            => this.titula = titula;
+    }
+    public class Student : Osoba
+    {
+        public override string Uloga => "Student";
+        public string jmbag = "0031231231";
+        public new void Info()
+        {
+            Console.Write(jmbag + " ");
+            base.Info();
+        }
+        //=> Console.WriteLine($"{ime} {prezime} {jmbag}");
+        public Student(string ime, string prezime, string jmbag)
+            : base(ime, prezime)
+            => this.jmbag = jmbag;
+    }
+    public class Honorarni : Zaposlenik
+    {
+        public decimal honorar;
+        public override void Info()
+        {
+            base.Info();
+            Console.WriteLine($"\thonorar: {honorar} EUR");
+        }
+        //=> Console.WriteLine($"{titula} {ime} {prezime} ({honorar} EUR)");
+        public Honorarni(string ime, string prezime, string titula,
+            decimal honorar) : base(ime, prezime, titula)
+            => this.honorar = honorar;
+    }
+}
---
+++ Tvrtka/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tvrtka;
+
+//Osoba w = new Osoba();
+Zaposlenik x = new Zaposlenik("Edgar","Poe","dr.sc.");
+Student y = new Student("Ann","Horvat","0012345677");
+Honorarni z = new Honorarni("Ivo","Ivic","mr.sc.",1000.123M);
+
+IspisSvih(x,y,z);
+y.Info();
+IspisUloge(x);
+IspisUloge(y);
+IspisUloge(z);
+
+static void IspisUloge(Osoba a)
+	=> Console.WriteLine(a.Uloga);
+
+static void IspisSvih(params Osoba[] a)
+{
+    Console.WriteLine("Ispis podataka:");
+	/* for (int i = 0; i < a.Length; i++)
+	{
+		a[i].Info();
+	} */
+	foreach (var o in a)
+		o.Info();
+}
